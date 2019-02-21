444e31cf483d945a98502f9c68d548362c9d31eb
Dave Beyer <dave.beyer@gmail.com>
2019-02-21 03:08:42 -0800

    Try markdown->html converter

--- assets/js/gvinit.js
+++ assets/js/gvinit.js
@@ -1,4 +1,14 @@
+let converter = new showdown.Converter();
 
-$(document).ready( () => {
-    $('.gv-only').hide();
+$(document).ready( function() {
+    $('.gv-only').each( function() {
+        let md   = $(this).text();
+        let html = converter.makeHtml(md);
+        $(this).text(html);
+    });
+
+    // Hide some content depending on query args
+//    $('.gv-only').hide();
+
 });
+
